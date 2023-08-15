7806978b7586c4a3e90bc9d8343f00655931f752
Mirco Valentini <mirco.valentini@ecmwf.int>
2023-08-15 23:14:59 +0200

    improved handling of parallel algorithms

--- src/multio/util/ParallelPolicies.h
+++ src/multio/util/ParallelPolicies.h
@@ -1,14 +1,25 @@
 #pragma once
 
-#include <iostream>
-#include <algorithm>
 #include "multio/multio_config.h"
+
+#include <iostream>
 
 
 // #define HAVE_EXECUTION_POLICY
 #ifdef HAVE_EXECUTION_POLICY
-#warning "Execution policies from c++17 enabled "
+#ifdef HAVE_GNU_COMPILER
+#warning "Execution policies from gnu c++17 enabled "
 #include <execution>
+#include <algorithm>
+#endif
+#ifdef HAVE_INTEL_COMPILER
+#warning "Execution policies from intel c++17 enabled "
+#include <oneapi/dpl/algorithm>
+#include <oneapi/dpl/execution>
+#endif
+#ifdef HAVE_CLANG_COMPILER
+#error "Execution policies from clang c++17 not tested"
+#endif
 #endif
 
 namespace multio::util {
@@ -26,14 +37,34 @@ template <typename Func>
 decltype(auto) dispatchExecutionPolicy(ExecutionPolicy t, Func&& f) {
     switch (t) {
         case (ExecutionPolicy::unseq):
+#ifdef HAVE_GNU_COMPILER
             return std::forward<Func>(f)(std::execution::unsequenced_policy{});
+#endif
+#ifdef HAVE_INTEL_COMPILER
+            return std::forward<Func>(f)(oneapi::dpl::execution::unsequenced_policy{});
+#endif
         case (ExecutionPolicy::par_unseq):
+#ifdef HAVE_GNU_COMPILER
             return std::forward<Func>(f)(std::execution::parallel_unsequenced_policy{});
+#endif
+#ifdef HAVE_INTEL_COMPILER
+            return std::forward<Func>(f)(oneapi::dpl::execution::parallel_unsequenced_policy{});
+#endif
         case (ExecutionPolicy::par):
+#ifdef HAVE_GNU_COMPILER
             return std::forward<Func>(f)(std::execution::parallel_policy{});
+#endif
+#ifdef HAVE_INTEL_COMPILER
+            return std::forward<Func>(f)(oneapi::dpl::execution::parallel_policy{});
+#endif
         case (ExecutionPolicy::seq):
         default:
+#ifdef HAVE_GNU_COMPILER
             return std::forward<Func>(f)(std::execution::sequenced_policy{});
+#endif
+#ifdef HAVE_INTEL_COMPILER
+            return std::forward<Func>(f)(oneapi::dpl::execution::sequenced_policy{});
+#endif
     }
 };
 #endif
